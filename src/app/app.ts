ee452fb4efe9de2a59bb21a4b81ea0369dda2092
Clayton Muhoza <muhozaclayton@gmail.com>
2025-09-17 10:25:28 +0200

    add material to event-card

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from './core/layout/header/header';
-import {EventList} from './event/event-list/event-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, EventList],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/event/event-card/event-card.ts
+++ src/app/event/event-card/event-card.ts
@@ -1,8 +1,12 @@
 import {Component, input} from '@angular/core';
 import  {EventSchema} from '../models/event-schema'
+import {MatCard, MatCardHeader} from '@angular/material/card';
 @Component({
   selector: 'app-event-card',
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader
+  ],
   templateUrl: './event-card.html',
   styleUrl: './event-card.css'
 })
