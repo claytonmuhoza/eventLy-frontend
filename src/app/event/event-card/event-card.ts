58a5d50d950940ffa6e10ede348fda300d9f62a8
Clayton Muhoza <muhozaclayton@gmail.com>
2025-09-17 17:49:42 +0200

    add action button to list-card

--- src/app/event/event-card/event-card.ts
+++ src/app/event/event-card/event-card.ts
@@ -1,13 +1,18 @@
 import {Component, input} from '@angular/core';
 import  {EventSchema} from '../models/event-schema'
-import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
+import {MatButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
 @Component({
   selector: 'app-event-card',
   imports: [
     MatCard,
     MatCardHeader,
     MatCardContent,
-    MatCardTitle
+    MatCardTitle,
+    MatCardActions,
+    MatButton,
+    MatIcon
   ],
   templateUrl: './event-card.html',
   styleUrl: './event-card.css'
