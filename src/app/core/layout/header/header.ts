e3d47ddcd2a029c3f247a9dd7e8b372aca6264d5
Clayton Muhoza <muhozaclayton@gmail.com>
2025-10-28 09:59:46 +0100

    chore: add animation to active link

--- src/app/core/layout/header/header.ts
+++ src/app/core/layout/header/header.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {MatToolbarRow} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    MatToolbarRow
   ],
   templateUrl: './header.html',
   styleUrl: './header.css'
